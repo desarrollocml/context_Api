7ba862f89126a5e3888f0d79fe3e8e770de371a9
desarrollocml <cristianmirandalamas@gmail.com>
2022-01-13 00:25:06 -0300

    userprovider

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
-import { useState } from "react";
 import MovieList from "./components/MovieList";
 import Navbar from "./components/Navbar";
-import UserContext from "./contexts/UserContext";
+import { UserProvider } from "./contexts/UserContext";
 
-const initialUser = {
-  id: 1,
-  name: "Luis",
-  favoriteMovies: [1, 2, 3],
-};
+
 function App() {
-  const [user, setUser] = useState(initialUser);
-  const login=()=>{
-    setUser(initialUser)
-  }
-  const logout=()=>{
-    setUser(null)
-  }
-  const data = { user, login, logout };
-
+  
   return (
     <div>
-      <UserContext.Provider value={data}>
+      <UserProvider>
         <Navbar></Navbar>
         <MovieList></MovieList>
-      </UserContext.Provider>
+      </UserProvider>
     </div>
   );
 }
--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,5 +1,21 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const UserContext = createContext();
 
+const initialUser = { id: 1, name: "Miguel", favoriteMovies: [1, 2] };
+
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(initialUser);
+
+  const login = () => {
+    setUser(initialUser);
+  };
+  const logout = () => {
+    setUser(null);
+  };
+
+  const data = { user, login, logout };
+  return <UserContext.Provider value={data}>{children}</UserContext.Provider>;
+};
+export {UserProvider}
 export default UserContext;
